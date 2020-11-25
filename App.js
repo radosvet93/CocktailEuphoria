fede0af519387e12a4685799fb6fc4e3ef197726
Radosvet Petrov <93radosvet@gmail.com>
2020-11-25 23:49:16 +0000

    Added bottom tabs

--- App.js
+++ App.js
@@ -2,13 +2,28 @@ import React from "react";
 import { ThemeProvider, Header } from "react-native-elements";
 import { ScrollView, SafeAreaView } from "react-native";
 import Constants from "expo-constants";
-import Main from "./app/components/Main";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet } from "react-native";
+
+import Search from "./app/components/Search";
+import Categories from "./app/components/Categories";
+import Random from "./app/components/Random";
 import Details from "./app/components/Details";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+const Home = ({ navigation }) => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Search">{(props) => <Search {...props} styles={styles} navigation={navigation} />}</Tab.Screen>
+      <Tab.Screen name="Categories">{(props) => <Categories {...props} styles={styles} navigation={navigation} />}</Tab.Screen>
+      <Tab.Screen name="Random">{(props) => <Random {...props} styles={styles} navigation={navigation} />}</Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 const theme = {
   Button: {
     raised: true,
@@ -21,11 +36,18 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Main" component={Main} options={{ title }} />
-          <Stack.Screen name="Details" component={Details} options={{ title }} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ title }} />
+          <Stack.Screen name="Details">{(props) => <Details {...props} styles={styles} />}</Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#e4e4e4",
+  },
+});
