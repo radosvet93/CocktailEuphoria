164a1097354792beacee151fd3899f244617eb94
Radosvet Petrov <93radosvet@gmail.com>
2020-11-25 23:51:49 +0000

    added safe area view

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Text, Button, ListItem, Avatar, Input } from "react-native-elements";
-import { View, ScrollView, SafeAreaView } from "react-native";
+import { Keyboard, View, ScrollView, SafeAreaView } from "react-native";
 import { useGetCocktailsByName } from "../hooks/useGetCocktailByName";
+import { Constants } from "react-native-unimodules";
 
 const Search = ({ styles, navigation }) => {
   const [cocktail, setCocktail] = useState("");
@@ -13,46 +14,49 @@ const Search = ({ styles, navigation }) => {
   }, []);
 
   return (
-    <>
-      <View>
-        <Text h3>Search for cocktails</Text>
-        <Input placeholder="Search for cocktail" onChangeText={(value) => setCocktail(value)} value={cocktail} ref={inputRef} />
-      </View>
-      <View style={styles}>
-        <Button
-          loading={isLoading}
-          onPress={() => {
-            getCocktails();
-            setCocktail("");
-          }}
-          title="Search"
-        />
-        {error || !cocktails ? (
-          <Text>{error}</Text>
-        ) : (
-          <>
-            {!!cocktails.length && <Text>Found {cocktails.length} results</Text>}
-            {cocktails.map((cocktail) => {
-              return (
-                <ListItem
-                  key={cocktail.idDrink}
-                  bottomDivider
-                  onPress={() => {
-                    navigation.navigate("Details", { cocktail });
-                  }}
-                >
-                  <Avatar size="xlarge" source={{ uri: cocktail.strDrinkThumb }} />
-                  <ListItem.Content>
-                    <ListItem.Title>{cocktail.strDrink}</ListItem.Title>
-                    <ListItem.Subtitle>{cocktail.strInstructions}</ListItem.Subtitle>
-                  </ListItem.Content>
-                </ListItem>
-              );
-            })}
-          </>
-        )}
-      </View>
-    </>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View>
+          <Text h3>Search for cocktails</Text>
+          <Input placeholder="Search for cocktail" onChangeText={(value) => setCocktail(value)} value={cocktail} ref={inputRef} />
+        </View>
+        <View>
+          <Button
+            loading={isLoading}
+            onPress={() => {
+              getCocktails();
+              setCocktail("");
+              Keyboard.dismiss;
+            }}
+            title="Search"
+          />
+          {error || !cocktails ? (
+            <Text>{error}</Text>
+          ) : (
+            <>
+              {!!cocktails.length && <Text>Found {cocktails.length} results</Text>}
+              {cocktails.map((cocktail) => {
+                return (
+                  <ListItem
+                    key={cocktail.idDrink}
+                    bottomDivider
+                    onPress={() => {
+                      navigation.navigate("Details", { cocktail });
+                    }}
+                  >
+                    <Avatar size="xlarge" source={{ uri: cocktail.strDrinkThumb }} />
+                    <ListItem.Content>
+                      <ListItem.Title>{cocktail.strDrink}</ListItem.Title>
+                      <ListItem.Subtitle>{cocktail.strInstructions}</ListItem.Subtitle>
+                    </ListItem.Content>
+                  </ListItem>
+                );
+              })}
+            </>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
